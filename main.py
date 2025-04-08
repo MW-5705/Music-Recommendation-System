6c1a9818f85d1643a471140c1969493320724c3f
Mehar Walia <meharwalia57@gmail.com>
2025-04-08 13:19:14 +0530

    New Try

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import requests
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+
+load_dotenv()
+
+auth_manager = SpotifyOAuth(client_id = '76868975d6304594b61e17963eb4983e', client_secret = 'cb7aa291d3d947a192eba10da7509ebe',
+redirect_uri = 'http://127.0.0.1:8888/callback', 
+scope = "user-read-private")
+
+s = spotipy.Spotify(auth_manager = auth_manager)
 
 with open('My Spotify Library.csv', 'r') as f:
     a = f.readlines()
@@ -7,7 +19,10 @@ with open('My Spotify Library.csv', 'r') as f:
         print (i[0], '  ', i[1], '   ', i[6])
 
 
-url = "https://api.spotify.com/v1/audio-features/7gl8XJ8EmiObfSFppTATt6"
-response = requests.get(url)
+# url = "https://api.spotify.com/v1/audio-features/7gl8XJ8EmiObfSFppTATt6"
+# response = requests.get(url)
+a = s.audio_analysis('3n3Ppam7vgaVa1iaRUc9Lp')
 
-print(response.status_code)
+print(a)
+
+
