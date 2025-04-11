2590fc233c60623892ff7d00f1c87213e259bb42
MW-5705 <113458487+MW-5705@users.noreply.github.com>
2025-04-11 20:11:11 +0530

    Safety commit

--- scrapper.py
+++ scrapper.py
@@ -1,34 +1,49 @@
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import requests
 import os
+import time
 
 listeners = 0
+scrobbles = 0
 
-with open('My Spotify Library.csv', 'r') as data:
+def converter(text):
+            if (text[-1] == 'K'):
+                return float(text[:-1])*1000
+            elif (text[-1] == 'M'):
+                return float(text[:-1])*1000000
+            else:
+                return float(text[:-1])        
+            
 
+with open('My Youtube Music Library.csv', 'r') as data:
     data.readline()
     songs = data.readlines()
     for i in songs:
         i = i.split(',')
-        i[0].replace(' ', '+')
-        print(i[0].replace(' ', '+'), "   ", i[1].replace(' ', '+'))
+        i[0] = i[0].split('-')
+        i[0][1] = i[0][1].strip()
+        i[0][0] = i[0][0].replace(',', ' ')
+        print(i[0][0].replace(' ', '+'), "   ", i[0][1].replace(' ', '+'))
         url = f"https://www.last.fm/music/{i[1][1:-1].replace(' ', '+')}/_/{i[0][1:-1].replace(' ', '+')}"
         print(url)
-        page = urlopen(url)
-
-        html_bytes = page.read()
-
-        html = html_bytes.decode("utf-8")
-        # print(type(html))
-
-        with open('Data.html', 'w', encoding='utf-8') as f:
-            f.write(html)
-        soup = BeautifulSoup(html, 'html.parser')
+        headers = {'User-Agent' : 'Mozilla/5.0'}
+        
+        req = requests.get(url=url, headers=headers, timeout=10)
+        soup = BeautifulSoup(req.text, 'html.parser')
         a = ((soup.find_all('abbr', class_= 'intabbr js-abbreviated-counter')))
         # print(a)
         # os.remove('Data.html')
-        listener = float(a[0].text[:-1])
-        listener += listener
+        listener_text = a[0].text
+        scrobble_text = a[1].text
+        listener_text = listener_text.replace(',', '')
+        scrobble_text = scrobble_text.replace(',', '')
+        scrobbles += converter(scrobble_text)
+        listeners += converter(listener_text)
+        time.sleep(2)
+        
+        
 print(listeners)
+print(scrobbles)
 
 # print(html)
+
